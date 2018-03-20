97448dc6c1f5b8e1314d25a65d09d121d44c3406
root <root@localhost.localdomain>
2018-03-20 09:28:22 -0500

    -

---
+++ FindElements/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FindElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        string[] daysOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        int indexOfW = Array.FindIndex(daysOfWeek, x => x[0] == 'W');
+        int indexOfTues = Array.IndexOf(daysOfWeek, "Tuesday");
+        string[] allWith6Chars = Array.FindAll(daysOfWeek, x=>x.Length == 6);
+        foreach(string item in allWith6Chars)
+        {
+          Console.WriteLine(item);
+        }
+        Console.WriteLine(indexOfTues);
+        Console.WriteLine(indexOfW);
+        Console.WriteLine(daysOfWeek[indexOfW]);
+        Console.WriteLine();
+        }
+    }
+}
