8487e8eb47f923e89f31a0a113d8990302b6fe50
root <root@localhost.localdomain>
2018-03-23 01:56:55 -0500

    -

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          string[] sortedDays = {"Friday", "Monday", "Saturday", "Sunday", "Thursday", "Tuesday", "Wednesday"};
+          int indexOfSun = Array.BinarySearch(sortedDays, "Sunday");
+          //int indexOfSun = Array.IndexOf(sortedDays, "Sunday");
+          Console.WriteLine("Index is " + indexOfSun);
+        
+        }
+    }
+}
