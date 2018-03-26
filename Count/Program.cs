ced1ac268db40ae276ab2cddbc2c7d500655ea09
Russell <russell@localhost.localdomain>
2018-03-26 15:17:35 -0500

    -

---
+++ Count/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+namespace Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          string[] daysOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+          ICollection<string> collection = (ICollection<string>)daysOfWeek;
+          Console.WriteLine("Count = " + collection.Count);
+          Console.WriteLine("Length = " + daysOfWeek.Length);
+        }
+    }
+}
