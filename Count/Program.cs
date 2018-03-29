b0d0cfdf093d5e00a6b9597e0eb447200e150d96
Russell <russell@localhost.localdomain>
2018-03-29 08:35:16 -0500

    -

--- Count/Program.cs
+++ Count/Program.cs
@@ -10,8 +10,13 @@ namespace Count
         {
           string[] daysOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
           ICollection<string> collection = (ICollection<string>)daysOfWeek;
+          Console.WriteLine("Count() = " + collection.Count());
+
           Console.WriteLine("Count = " + collection.Count);
+
           Console.WriteLine("Length = " + daysOfWeek.Length);
+
+          collection.Add("SlaveDay");
         }
     }
 }
