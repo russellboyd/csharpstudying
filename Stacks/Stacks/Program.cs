0370cc39fa3706c16550d7e99c225df2833de81d
Russell <rgboyd10@gmail.com>
2018-04-05 22:07:00 -0500

    '-'

---
+++ Stacks/Stacks/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> books = new Stack<string>();
+            books.Push("Programming WPF");
+            books.Push("The Philosophy Book");
+            books.Push("Heat and Thermodynamics");
+            books.Push("Harry Potter and the Chamber of Secrets");
+
+            Console.WriteLine("ALL BOOKS: ");
+            foreach (string title in books)
+            {
+                Console.WriteLine(title);
+            }
+        }
+    }
+}
