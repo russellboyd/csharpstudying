6100890cdd1e8e09c9c0e134e7bf6d652e1043f1
Russell <rgboyd10@gmail.com>
2018-05-01 10:58:39 -0500

    '-'

---
+++ JaggedArrays/JaggedArrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[][] tempsGrid = new float[4][];
+            for (int x = 0; x < 4; x++)
+            {
+                tempsGrid[x] = new float[3];
+                for (int y = 0; y < 3; y++)
+                {
+                    tempsGrid[x][y] = x + 10 * y;
+                }
+            }
+
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    Console.WriteLine(tempsGrid[x][y] + ", ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ MultiDimensionalArray/MultiDimensionalArray/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiDimensionalArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[,] tempsGrid = new float[4, 3];
+            //float[] array1d = tempsGrid; // can't be done
+
+            Console.WriteLine("Length is " + tempsGrid.Length);
+            Console.WriteLine("Rank is " + tempsGrid.Rank);
+
+            for (int x = tempsGrid.GetLowerBound(0); x <= tempsGrid.GetUpperBound(0); x++)
+            {
+                for (int y = tempsGrid.GetLowerBound(1); y <= tempsGrid.GetUpperBound(1); y++)
+                {
+                    tempsGrid[x, y] = x + 10 * y;
+                }
+            }
+
+            foreach (float temperature in tempsGrid)
+            {
+                Console.WriteLine(temperature);
+            }
+
+            for (int x = 0; x < tempsGrid.GetLength(0); x++)
+            {
+                for (int y = 0; y < tempsGrid.GetLength(1); y++)
+                {
+                    Console.Write(tempsGrid[x,y] + ", ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
