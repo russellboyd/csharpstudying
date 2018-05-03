543b10eb61b116c5b05cca85b8ea369e183ddb36
Russell <rgboyd10@gmail.com>
2018-05-02 22:15:29 -0500

    '-'

---
+++ EqualButtons/EqualButtons/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EqualButtons
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Button button1 = new Button();
+            button1.Text = "Click me now!";
+
+            Button button2 = new Button();
+            button2.Text = "Click me now!";
+
+            Console.WriteLine(button1 == button2);
+        }
+    }
+}
---
+++ EqualInts/EqualInts/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualInts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int three = 3;
+            int threeAgain = 3;
+            bool intCmp = three == threeAgain;
+            Console.WriteLine(string.Format("Compare ints : {0}", intCmp));
+
+            bool objCmp = (object)three == (object)threeAgain;
+            Console.WriteLine(string.Format("compare objects : {0}",objCmp));
+
+            //objects are reference types which is why objCmp is false.  the integers have been boxed into different reference instances.
+
+            bool itfCmp = ((IComparable<int>)three == (IComparable<int>)threeAgain);
+            Console.WriteLine(string.Format("Compare interfaces : {0}", itfCmp));
+
+
+        }
+    }
+}
