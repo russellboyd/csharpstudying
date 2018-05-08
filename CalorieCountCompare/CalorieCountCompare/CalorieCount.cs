a49f6a3afa219b8b83b8e94842cbf41a7ae0839f
Russell <rgboyd10@gmail.com>
2018-05-08 17:10:05 -0500

    '-'

--- CalorieCountCompare/CalorieCountCompare/CalorieCount.cs
+++ CalorieCountCompare/CalorieCountCompare/CalorieCount.cs
@@ -6,8 +6,53 @@ using System.Threading.Tasks;
 
 namespace CalorieCountCompare
 {
-    public struct CalorieCount
+    public struct CalorieCount : IComparable<CalorieCount>
     {
+        public int CompareTo(CalorieCount other)
+        {
+            return this._value.CompareTo(other._value);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            if (!(obj is CalorieCount))
+                return false;
+            return _value == ((CalorieCount)obj)._value;
+        }
+
+        public override int GetHashCode()
+        {
+            return _value.GetHashCode();
+        }
+        public static bool operator <(CalorieCount x, CalorieCount y)
+        {
+            return x._value < y._value;
+        }
+
+        public static bool operator <=(CalorieCount x, CalorieCount y)
+        {
+            return x._value <= y._value;
+        }
+
+        public static bool operator >(CalorieCount x, CalorieCount y)
+        {
+            return x._value > y._value;
+        }
+        public static bool operator >=(CalorieCount x, CalorieCount y)
+        {
+            return x._value >= y._value;
+        }
+        public static bool operator ==(CalorieCount x, CalorieCount y)
+        {
+            return x._value == y._value;
+        }
+        public static bool operator !=(CalorieCount x, CalorieCount y)
+        {
+            return x._value != y._value;
+        }
+
         private float _value;
         public float Value { get { return _value; } }
 
--- CalorieCountCompare/CalorieCountCompare/Program.cs
+++ CalorieCountCompare/CalorieCountCompare/Program.cs
@@ -10,6 +10,29 @@ namespace CalorieCountCompare
     {
         static void Main(string[] args)
         {
+            CalorieCount cal300 = new CalorieCount(300);
+            CalorieCount cal400 = new CalorieCount(400);
+
+            DisplayOrder(cal300, cal400);
+            DisplayOrder(cal400, cal300);
+            DisplayOrder(cal300, cal300);
+
+            if (cal300 < cal400)
+            {
+                Console.WriteLine("cal300 < cal400");
+            }
+
+        }
+
+        static void DisplayOrder<T>(T x, T y) where T : IComparable<T>
+        {
+            int result = x.CompareTo(y);
+            if (result == 0)
+                Console.WriteLine("{0,12} = {1}", x, y);
+            else if (result > 0)
+                Console.WriteLine("{0,12} > {1}", x, y);
+            else
+
         }
     }
 }
