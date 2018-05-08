b277be888ce4764bce5774bbdd9467c25436b42a
Russell <rgboyd10@gmail.com>
2018-05-08 15:28:17 -0500

    '-'

---
+++ CalorieCountCompare/CalorieCountCompare/CalorieCount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalorieCountCompare
+{
+    public struct CalorieCount
+    {
+        private float _value;
+        public float Value { get { return _value; } }
+
+        public CalorieCount(float value)
+        {
+            this._value = value;
+        }
+
+        public override string ToString()
+        {
+            return _value + " cal";
+        }
+    }
+}
