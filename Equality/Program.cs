4c2811337f45a65771d4d4bd5510987dbfad0f05
Russell Boyd <rgboyd10@gmail.com>
2018-05-18 15:15:06 -0500

    '-'

---
+++ Equality/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Equality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] list = {"orange", "banana", "pear", "apple"};
+	    Array.Sort(list);
+
+	    foreach(var item in list)
+	    	Console.WriteLine(item);
+        }
+    }
+}
