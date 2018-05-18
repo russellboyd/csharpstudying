869614f9a116c7f678ba951ccd47e2b73415d826
Russell Boyd <rgboyd10@gmail.com>
2018-05-18 11:30:50 -0500

    '-'

---
+++ StringEquality/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace StringEquality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		bool areEqual = string.Equals("Apple", "Pineapple", StringComparison.CurrentCultureIgnoreCase);
+		int cmpResult = string.Compare("Apple", "Pineapple", CultureInfo.GetCultureInfo("fr-FR"),CompareOptions.IgnoreSymbols);
+		areEqual = (cmpResult == 0);
+
+		//case-sensitive ordinal comparison
+		areEqual = ("Apple" == "Pineapple");
+		areEqual = "Apple".Equals("Pineapple");
+	}
+    }
+}
