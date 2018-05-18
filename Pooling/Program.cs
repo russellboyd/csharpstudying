e9cc74247ddcd5ccffcf77c5bfc4fbcc9bd9cb5a
Russell Boyd <rgboyd10@gmail.com>
2018-05-18 14:22:00 -0500

    '-'

---
+++ Pooling/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Pooling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		string apple1 = "Apple";
+		string apple2 = "Ap" + "ple";
+
+		Console.WriteLine(string.Format("strings are {0} and {1}", apple1, apple2));
+
+		Console.WriteLine(apple1 == apple2);
+		Console.WriteLine(ReferenceEquals(apple1, apple2));
+        }
+    }
+}
