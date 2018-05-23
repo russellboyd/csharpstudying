fd6824ef82b807805d52f83e61d05409a073059e
russgbiv <rgboyd10@gmail.com>
2018-05-23 14:43:00 -0500

    '-'

---
+++ StringCompare/StringComparer/StringComparer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Globalization;
+
+namespace StringComparer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new HashSet<string>(StringComparer.CurrentCultureIgnoreCase);
+            names.Add("apple");
+            names.Add("pear");
+            names.Add("pineapple");
+            names.Add("Apple");
+
+            foreach (string name in names)
+                Console.WriteLine(name);
+
+
+        }
+    }
+}
---
+++ StructuralEquality/StructuralEquality/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructuralEquality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr1 = { "apple", "orange", "pineapple" };
+            string[] arr2 = { "apple", "pear", "pineapple" };
+
+            //Console.WriteLine(arr1 == arr2);
+            //Console.WriteLine(arr1.Equals(arr2));
+            var arrayEq = (IStructuralEquatable)arr1;
+            bool structEqual = arrayEq.Equals(arr2, StringComparer.OrdinalIgnoreCase);
+            Console.WriteLine(structEqual);
+
+            var arrayComp = (IStructuralComparable)arr1;
+            int structComp = arrayComp.CompareTo(arr2, StringComparer.OrdinalIgnoreCase);
+            Console.WriteLine(structComp);
+        }
+
+    }
+}
