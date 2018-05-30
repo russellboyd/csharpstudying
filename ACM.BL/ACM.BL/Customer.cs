2a6db99bf7c22527c400129fe0b2b576b4bfcf7d
russgbiv <rgboyd10@gmail.com>
2018-05-30 14:20:46 -0500

    '-'

---
+++ ACM.BL/ACM.BL/Customer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public Customer()
+        { }
+
+        public Customer(int customerId)
+        {
+            this.CustomerId = customerId;
+        }
+        public static int InstanceCount { get; set; } 
+        private string _lastname;
+        public string LastName
+        {
+            get { return _lastname; }
+            set { _lastname = value; }
+        }
+
+        public string FirstName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public int CustomerId { get; private set; }
+
+        public string FullName
+        {
+            get
+            {
+                string fullName = LastName;
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrWhiteSpace(fullName))
+                    {
+                        fullName += ", ";
+                    }
+                    fullName += FirstName;
+                }
+                return fullName;
+            }
+        }
+
+        public Customer Retrieve(int customerId)
+        {
+            //Code that retrieves the defined customer
+            return new Customer();
+        }
+
+        public List<Customer> Retrieve()
+        {
+            return new List<Customer>();
+        }
+
+        public bool Save()
+        {
+            //code that saves the defined customer
+            return true;
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (String.IsNullOrWhiteSpace(LastName)) isValid = false;
+            if (string.IsNullOrWhiteSpace(EmailAddress)) isValid = false;
+
+            return isValid;
+
+            
+        }
+
+    }
+}
